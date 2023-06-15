88a659871f34e8b90bd320b92749d1a74d60a3ce
Yassirnih <yassirnih@gmail.com>
2023-06-15 15:31:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,13 +9,8 @@
  *
  */
 
-int main(void) {
-
-	// initializing strings
-	char str1[] = "\"Programming is like building a multilingual puzzle";
-
-	// writing to stdout
-	puts(str1);
-
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
