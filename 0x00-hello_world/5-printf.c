4e46358253a6d8fabdc767d9f4299b9a1e1c865e
Yassirnih <yassirnih@gmail.com>
2023-06-15 15:40:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * task - prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: return value is 0 which means always succesful
+ */
+
+int main(void){
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return 0;
+}
