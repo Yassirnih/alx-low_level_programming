b53582e3e1d156fe8df35b99e6b81274e518b80f
Yassirnih <yassirnih@gmail.com>
2023-06-16 17:12:13 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,7 +13,7 @@ int main(void)
 	int alphabet;
 
 	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
-	       	putchar(alphabet);
+		putchar(alphabet);
 	for (alphabet = 'A' ; alphabet <= 'Z' ; alphabet++)
 		putchar(alphabet);
 	putchar('\n');
