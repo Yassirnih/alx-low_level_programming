3b022a97b2fcfcaef04d3b1449dee88dcc2ff660
Yassirnih <yassirnih@gmail.com>
2023-06-16 17:23:12 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * task - prints alphabets
+ *
+ * Return: return value is 0 which means always succesful
+ */
+
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+		if (alphabet == 'q')
+	{
+			alphabet++;
+	}
+		else if (alphabet == 'e')
+	{
+			alphabet++;
+	}
+		putchar(alphabet);
+	putchar('\n');
+
+	return (0);
+}
