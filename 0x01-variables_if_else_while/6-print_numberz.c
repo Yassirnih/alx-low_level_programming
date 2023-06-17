752acac8ff91aa0cd708ec2a7704bacb8a9bdff0
Yassirnih <yassirnih@gmail.com>
2023-06-16 17:54:36 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * task - prints single digits
+ *
+ * Return: return value is 0 which means always succesful
+ */
+
+int main(void)
+{
+	int a = '0';
+
+	for (a = '0' ; a <= '9' ; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
