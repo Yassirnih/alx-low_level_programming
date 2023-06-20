915ce69b36073d9f6a0e98fed94de95bda5df8cf
Yassirnih <yassirnih@gmail.com>
2023-06-20 15:57:38 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - variable
+ * @num: getting abs
+ * Return: -n the negative value and n the prositive
+ */
+
+int _abs(int)
+{
+	int num;
+
+	if (num <= 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
