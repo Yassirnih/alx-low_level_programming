e77642578902145406513591c6dfb9b7b67f533f
Yassirnih <yassirnih@gmail.com>
2023-06-20 15:50:05 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - variable
- * @c: checking number sign
+ * @n: checking number sign
  * Return: 1 if 'c' lowercase, otherwise 0.
  */
 
@@ -12,7 +12,7 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	}	
+	}
 	else if (n == 0)
 	{
 		_putchar('0');
@@ -22,5 +22,5 @@ int print_sign(int n)
 	{
 		_putchar ('-');
 	}
-		return (-1);	
+		return (-1);
 }
