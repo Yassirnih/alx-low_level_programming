0916dd19d8e05fce2be11297354dea017186e80a
Yassirnih <yassirnih@gmail.com>
2023-06-20 13:51:08 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,12 @@
 /**
  * _islower - variable
  * Description: checking if char lowercase or not
- * Return: 1 if "c" lowercase, otherwise 0.
+ * Return: 1 if 'c' lowercase, otherwise 0.
  */
 
 int _islower(int c)
 {
+	int c;
 	if (c >= 97 && c <= 122)
 		return (1);
 	else
