ba2c4498943ec083cab04b964f4e42f5fe3139f9
Yassirnih <yassirnih@gmail.com>
2023-06-20 16:33:35 -0700

    last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - variable
+ * @num: printing the last num
+ * Return: the lastnum
+ */
+
+int print_last_digit(int num)
+{
+	int Lastnum;
+	if (num > 0)
+		Lastnum = num % 10;
+	else
+		Lasnum = (num % 10) * (-1)
+	_putchar(Lastnum + '0')
+	return (Lastnum);
+}
