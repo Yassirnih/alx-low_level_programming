a9f2b92b65b26f52a3ed60d1768bc673e64809ef
Yassirnih <yassirnih@gmail.com>
2023-06-20 10:26:08 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - putchar task
+ *
+ * return: 0 (succesful)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
