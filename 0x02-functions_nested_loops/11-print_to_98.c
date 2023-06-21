338c55becf5459d308c88668c6dd53cbaa8b0332
Yassirnih <yassirnih@gmail.com>
2023-06-20 17:12:45 -0700

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,7 @@
 
 /**
  * print_to_98 - variable
+ * @n: input number
  */
 
 void print_to_98(int n)
@@ -9,7 +10,7 @@ void print_to_98(int n)
 		int num;
 
 	if (n > 98)
-		for (num = n; num > 98; num++)
+		for (num = n; num > 98; num--)
 			printf("%d, ", num);
 	else
 		for (num = n; num < 98; num++)
