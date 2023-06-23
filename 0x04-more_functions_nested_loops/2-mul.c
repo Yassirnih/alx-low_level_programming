77310c16eda031934e369a8e1a71fc1b0611b57c
Yassirnih <yassirnih@gmail.com>
2023-06-22 18:05:13 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - 2 numbers multiplication
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: the outcome
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
