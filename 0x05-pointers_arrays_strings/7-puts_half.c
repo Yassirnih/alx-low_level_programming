76e65afd396efd1f1f1128ee0835c7103283e4d7
Yassirnih <yassirnih@gmail.com>
2023-06-26 18:29:39 -0700

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,19 +7,19 @@
 
 void puts_half(char *str)
 {
-	int i, j, max;
+	int a, n, b;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	max = i;
-	j = max / 2;
-	while (j <= max)
-	{
-		_putchar(str[j]);
-		j++;
-	}
+	b = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		b++;
+
+	n = (b / 2);
+
+	if ((b % 2) == 1)
+		n = ((b + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
 	_putchar('\n');
 }
