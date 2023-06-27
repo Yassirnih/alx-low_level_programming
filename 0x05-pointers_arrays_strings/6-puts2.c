3f434c712af5f0af28374d84c4762d707620ef34
Yassirnih <yassirnih@gmail.com>
2023-06-26 18:15:02 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - ENtry point
+ * @str: input
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	a = 0;
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+			_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
