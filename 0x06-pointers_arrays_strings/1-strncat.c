7a92f819fcb105bcf4d775f6eda24b7cdc774f5a
Yassirnih <yassirnih@gmail.com>
2023-06-29 17:16:49 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - ENtry point
+ *
+ * @dest: first input
+ * @src: second input
+ * @n: third input
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int len = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[len + i] = src[i];
+	dest[len + i] = '\0';
+
+	return (dest);
+}
